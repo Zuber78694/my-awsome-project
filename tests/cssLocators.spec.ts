f1a92c547baac02e537546853c5c1bd228e1c75f
zuber <zuberma0@gmail.com>
2026-06-26 23:00:01 +0530

    save

--- tests/cssLocators.spec.ts
+++ tests/cssLocators.spec.ts
@@ -4,7 +4,7 @@ test('CSS Locators', async ({ page }) => {
     await page.goto('https://demowebshop.tricentis.com/');
     //await page.waitForLoadState('domcontentloaded'); 
     //tagname#id,tagname.classname,tagname[attribute=value],tagname[attribute*=value],tagname[attribute^=value],tagname[attribute$=value]
-    await page.locator('a.ico-register').click();
+    //await page.locator('a.ico-register').click();
    expect (page.getByAltText('Tricentis Demo Web Shop')).toBeVisible(); 
     await page.locator('input[Search]').click(); 
     page.close();
