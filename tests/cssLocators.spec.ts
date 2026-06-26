fd8034d2eaca68fb9966a30278e2ff1fdfff8ea9
zuber <zuberma0@gmail.com>
2026-06-26 23:03:32 +0530

    tt

--- tests/cssLocators.spec.ts
+++ tests/cssLocators.spec.ts
@@ -5,7 +5,7 @@ test('CSS Locators', async ({ page }) => {
     //await page.waitForLoadState('domcontentloaded'); 
     //tagname#id,tagname.classname,tagname[attribute=value],tagname[attribute*=value],tagname[attribute^=value],tagname[attribute$=value]
     //await page.locator('a.ico-register').click();
-   expect (page.getByAltText('Tricentis Demo Web Shop')).toBeVisible(); 
+   //expect (page.getByAltText('Tricentis Demo Web Shop')).toBeVisible(); 
     await page.locator('input[Search]').click(); 
     page.close();
 }) 
