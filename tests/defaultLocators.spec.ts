a0da81bea514dc544b01abe158683279a107c4f1
zuber <zuberma0@gmail.com>
2026-06-26 22:33:16 +0530

    save

--- tests/defaultLocators.spec.ts
+++ tests/defaultLocators.spec.ts
@@ -1,11 +1,11 @@
 import { test, expect, Locator } from '@playwright/test';
 
 test('Locators', async ({ page }) => {
-    //await page.goto('https://www.naukri.com/');
+    await page.goto('https://www.naukri.com/');
     //await page.waitForLoadState('domcontentloaded');
     //await page.getByText('Login',{exact: true }).click() ; 
     //await page.getByRole('link', { name: 'Login' });--different roles in docs -role names
-  await page.goto('file:///C:/Users/Dell/OneDrive/Desktop/Zuber/PlayWright/Playwright/Pavan%20Kumar/app.html');
+  //await page.goto('file:///C:/Users/Dell/OneDrive/Desktop/Zuber/PlayWright/Playwright/Pavan%20Kumar/app.html');
    await page.waitForLoadState('domcontentloaded');
    //expect(await page.getByText('Playwright Locators Demonstration')).toBeTruthy();
    //page.getByLabel('Email Address:');
